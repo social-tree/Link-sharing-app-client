4ad780e2bf7c1bfc90a7dda741a753e6d94d8880
seikou <seikou615@gmail.com>
2023-07-18 08:27:52 +0100

    Exported svg icons as ReactComponent

---
+++ src/Icons/index.ts
@@ -0,0 +1,27 @@
+
+export { ReactComponent as ArrowRightIcon } from "./icon-arrow-right.svg";
+export { ReactComponent as ChangesSavedIcon } from "./icon-changes-saved.svg";
+export { ReactComponent as ChevronDownIcon } from "./icon-chevron-down.svg";
+export { ReactComponent as CodePenIcon } from "./icon-codepen.svg";
+export { ReactComponent as CodeWarsIcon } from "./icon-codewars.svg";
+export { ReactComponent as DevToIcon } from "./icon-devto.svg";
+export { ReactComponent as DragAndDropIcon } from "./icon-drag-and-drop.svg";
+export { ReactComponent as EmailIcon } from "./icon-email.svg";
+export { ReactComponent as FacebookIcon } from "./icon-facebook.svg";
+export { ReactComponent as FreeCodeCampIcon } from "./icon-freecodecamp.svg";
+export { ReactComponent as FrontEndMentorIcon } from "./icon-frontend-mentor.svg";
+export { ReactComponent as GitHubIcon } from "./icon-github.svg";
+export { ReactComponent as GitLabIcon } from "./icon-gitlab.svg";
+export { ReactComponent as HashnodeIcon } from "./icon-hashnode.svg";
+export { ReactComponent as LinkCopiedIcon } from "./icon-link-copied-to-clipboard.svg";
+export { ReactComponent as LinkIcon } from "./icon-link.svg";
+export { ReactComponent as LinkedInIcon } from "./icon-linkedin.svg";
+export { ReactComponent as LinksHeaderIcon } from "./icon-links-header.svg";
+export { ReactComponent as PasswordIcon } from "./icon-password.svg";
+export { ReactComponent as PreviewHeaderIcon } from "./icon-preview-header.svg";
+export { ReactComponent as ProfileDetailsHeaderIcon } from "./icon-profile-details-header.svg";
+export { ReactComponent as StackOverflowIcon } from "./icon-stack-overflow.svg";
+export { ReactComponent as TwitchIcon } from "./icon-twitch.svg";
+export { ReactComponent as TwitterIcon } from "./icon-twitter.svg";
+export { ReactComponent as UploadImageIcon } from "./icon-upload-image.svg";
+export { ReactComponent as YouTubeIcon } from "./icon-youtube.svg";
