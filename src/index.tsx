f409cbeea333344b2ad810e61a4cff8b0d098138
seikou <seikou615@gmail.com>
2023-07-24 14:43:10 +0100

    Wrapped App with BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import App from './App'
-import { GlobalStyle } from 'Styles'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import reportWebVitals from './reportWebVitals'
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "Styles";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
 
-reportWebVitals()
+reportWebVitals();
